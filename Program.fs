53c251c3b14f400d66a928bc11830469433b3515
cabboose <shayanftw@gmail.com>
2025-06-06 04:02:46 +0800

    Finalise 1.0.0 docs

--- Program.fs
+++ Program.fs
@@ -20,19 +20,24 @@ let [<Literal>] ext = ".mdx"
 
 [<RequireQualifiedAccess>]
 type Pages =
-    | Introduction | Installation
+    | Introduction
+    | Installation
     | OxpeckerFork
-    | Overview | SolidTypeComponent
-    | Spread | ContextProviders
-    | Polymorphism | SpecialBuilders
-    | Debugging | CommonIssues
-    | Plugin | Setters | Bindings
-    | Experimental | BuilderInterfaces
-    | Components | Terminology
-    | ModularForms | Motion
-    | Kobalte | Lucide | Cmdk
-    | ApexCharts | TanStackTable
-    | Primitives | NeoDrag | Storybook
+    | Overview
+    | Spread
+    | Polymorphism
+    | Bindings
+    | Experimental
+    | ModularForms
+    | Motion
+    | Kobalte
+    | Lucide
+    | Cmdk
+    | ApexCharts
+    | TanStackTable
+    | Primitives
+    | NeoDrag
+    | Storybook
     | CompiledOutput
     | SolidComponentAttribute
     | SolidTypeComponentAttribute
@@ -43,16 +48,11 @@ type Pages =
     | Contributing
     | Interfaces
     | StyleHelpers
-    | Elmish
     [<SolidComponent>]
     member this.createNavigationItem (?icon, ?version: string) =
         match this with
         | OxpeckerFork -> "Why Fork Oxpecker?"
-        | ContextProviders -> "Context Providers"
         | Overview -> "Usage Overview"
-        | SpecialBuilders -> "Custom Builders"
-        | CommonIssues -> "Common Issues"
-        | BuilderInterfaces -> "Prebuilt Builder Interfaces"
         | CompiledOutput -> "Advantages of JSX Output"
         | ApiDifferences -> "Solid-JS API Differences"
         | ComponentFlags -> "Component Flags"
@@ -75,34 +75,14 @@ type Pages =
             import def (pg + "Installation" + ext)
         | Overview ->
             import def "./pages/Overview.mdx"
-        | SolidTypeComponent ->
-            import def "./pages/SolidTypeComponent.mdx"
         | Spread ->
             import def "./pages/Spread.mdx"
-        | ContextProviders ->
-            import def "./pages/ContextProviders.mdx"
         | Polymorphism ->
             import def "./pages/Polymorphism.mdx"
-        | SpecialBuilders ->
-            import def "./pages/SpecialBuilders.mdx"
-        | Debugging ->
-            import def "./pages/Debugging.mdx"
-        | CommonIssues ->
-            import def "./pages/CommonIssues.mdx"
-        | Plugin ->
-            import def "./pages/Plugin.mdx"
-        | Setters ->
-            import def "./pages/Setters.mdx"
         | Bindings ->
             import def "./pages/Bindings.mdx"
         | Experimental ->
             import def "./pages/Experimental.mdx"
-        | BuilderInterfaces ->
-            import def "./pages/BuilderInterfaces.mdx"
-        | Components ->
-            import def "./pages/Components.mdx"
-        | Terminology ->
-            import def "./pages/Terminology.mdx"
         | ModularForms ->
             import def "./pages/ModularForms.mdx"
         | Motion ->
@@ -145,8 +125,6 @@ type Pages =
             import def (pg + "Interfaces" + ext)
         | StyleHelpers ->
             import def (pg + "StyleHelpers" + ext)
-        | Elmish ->
-            import def (pg + "Elmish" + ext)
 
 [<SolidComponent>]
 let GithubVisit () =
@@ -209,20 +187,10 @@ let Root () =
         PageRoute Pages.Introduction
         PageRoute Pages.Installation
         PageRoute Pages.Overview
-        PageRoute Pages.SolidTypeComponent
         PageRoute Pages.Spread
-        PageRoute Pages.ContextProviders
         PageRoute Pages.Polymorphism
-        PageRoute Pages.SpecialBuilders
-        PageRoute Pages.Debugging
-        PageRoute Pages.CommonIssues
-        PageRoute Pages.Plugin
-        PageRoute Pages.Setters
         PageRoute Pages.Bindings
         PageRoute Pages.Experimental
-        PageRoute Pages.BuilderInterfaces
-        PageRoute Pages.Components
-        PageRoute Pages.Terminology
         PageRoute Pages.ModularForms
         PageRoute Pages.Motion
         PageRoute Pages.Kobalte
@@ -244,55 +212,34 @@ let Root () =
         PageRoute Pages.Contributing
         PageRoute Pages.Interfaces
         PageRoute Pages.StyleHelpers
-        PageRoute Pages.Elmish
     }
 
 Data.Navigation.store.Update [|
-    NavigationGroup.create "Finished Pages" None [|
-        Pages.Introduction.createNavigationItem()
-        Pages.Installation.createNavigationItem()
-        Pages.Overview.createNavigationItem()
-        Pages.CompiledOutput.createNavigationItem()
-        Pages.Motion.createNavigationItem(version = "0.2.1")
-        Pages.Kobalte.createNavigationItem(version = "0.2.0")        
-        Pages.ApexCharts.createNavigationItem(version = "0.2.0")
-        Pages.Storybook.createNavigationItem(version = "0.1.2")
-    |]
     NavigationGroup.create "" None [|
         Pages.Introduction.createNavigationItem()
+        Pages.Overview.createNavigationItem()
         Pages.OxpeckerFork.createNavigationItem()
         Pages.Installation.createNavigationItem()
-        Pages.Overview.createNavigationItem()
         Pages.CompiledOutput.createNavigationItem()
+        Pages.ApiDifferences.createNavigationItem()
+    |]
+    NavigationGroup.create "Guides" None [|
         Pages.SolidComponentAttribute.createNavigationItem()
         Pages.SolidTypeComponentAttribute.createNavigationItem()
         Pages.PartasImportAttribute.createNavigationItem()
         Pages.ComponentFlags.createNavigationItem()
-        Pages.ApiDifferences.createNavigationItem()
-        Pages.Troubleshooting.createNavigationItem()
-        Pages.Contributing.createNavigationItem()
-        Pages.Interfaces.createNavigationItem()
-        Pages.StyleHelpers.createNavigationItem()
-        Pages.Elmish.createNavigationItem()
-    |]
-    NavigationGroup.create "Guides" None [|
-        Pages.SolidTypeComponent.createNavigationItem()
         Pages.Spread.createNavigationItem()
-        Pages.ContextProviders.createNavigationItem()
+        Pages.Interfaces.createNavigationItem()
         Pages.Polymorphism.createNavigationItem()
-        Pages.SpecialBuilders.createNavigationItem()
-        Pages.BuilderInterfaces.createNavigationItem()
-        Pages.Setters.createNavigationItem()
+        Pages.StyleHelpers.createNavigationItem()
         Pages.Experimental.createNavigationItem()
-        Pages.Components.createNavigationItem()
     |]
     NavigationGroup.create "Issue Reporting" None [|
-        Pages.Debugging.createNavigationItem()
-        Pages.CommonIssues.createNavigationItem()
+        Pages.Troubleshooting.createNavigationItem()
     |]
     NavigationGroup.create "Dev/Contributing" None [|
-        Pages.Plugin.createNavigationItem()
         Pages.Bindings.createNavigationItem()
+        Pages.Contributing.createNavigationItem()
     |]
     NavigationGroup.create "Bindings" None [|
         Pages.Primitives.createNavigationItem()
@@ -300,7 +247,7 @@ Data.Navigation.store.Update [|
         Pages.Kobalte.createNavigationItem(version = "0.2.0")
         Pages.ApexCharts.createNavigationItem(version = "0.2.0")
         Pages.Cmdk.createNavigationItem(version = "0.2.0")
-        Pages.Lucide.createNavigationItem(version = "0.2.0")
+        Pages.Lucide.createNavigationItem(version = "0.514.0")
         Pages.ModularForms.createNavigationItem(version = "0.2.0")
         Pages.NeoDrag.createNavigationItem(version = "0.2.0")
         Pages.TanStackTable.createNavigationItem(version = "0.2.0")
